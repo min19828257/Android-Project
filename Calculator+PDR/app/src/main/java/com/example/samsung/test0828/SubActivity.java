f89219d352fe075b4d2a574ee85b29f96311e5ea
김민수 <36617103+min19828257@users.noreply.github.com>
2018-09-08 17:27:49 +0900

    Today is 0908
    Calculator + PDR system

--- Calculator+PDR/app/src/main/java/com/example/samsung/test0828/SubActivity.java
+++ Calculator+PDR/app/src/main/java/com/example/samsung/test0828/SubActivity.java
@@ -109,7 +109,7 @@ public class SubActivity extends AppCompatActivity {
             }
         });
     }
-
+/*
     // 앱 종료시
     @Override
     protected void onStop() {
@@ -130,7 +130,7 @@ public class SubActivity extends AppCompatActivity {
         Log.e("LOG", "onPause()");
         mSensorManager.unregisterListener(mGyroLis);
     }
-
+*/
     @Override
     public void onDestroy(){
         super.onDestroy();
@@ -138,32 +138,29 @@ public class SubActivity extends AppCompatActivity {
         mSensorManager.unregisterListener(mGyroLis);
     }
 
+
     //자이로센서 리스너
     private class GyroscopeListener implements SensorEventListener {
+
         @Override
         public void onSensorChanged(SensorEvent event) {
-
             /* 각 축의 각속도 성분을 받는다. */
             double gyroX = event.values[0];
             double gyroY = event.values[1];
             double gyroZ = event.values[2];
-
             /* 각속도를 적분하여 회전각을 추출하기 위해 적분 간격(dt)을 구한다.
              * dt : 센서가 현재 상태를 감지하는 시간 간격
              * NS2S : nano second -> second */
             dt = (event.timestamp - timestamp) * NS2S;
             timestamp = event.timestamp;
-
             /* 맨 센서 인식을 활성화 하여 처음 timestamp가 0일때는 dt값이 올바르지 않으므로 넘어간다. */
             if (dt - timestamp * NS2S != 0) {
-
                 /* 각속도 성분을 적분 -> 회전각(pitch, roll)으로 변환.
                  * 여기까지의 pitch, roll의 단위는 '라디안'이다.
                  * SO 아래 로그 출력부분에서 멤버변수 'RAD2DGR'를 곱해주어 degree로 변환해줌.  */
                 pitch = pitch + gyroY * dt;
                 roll = roll + gyroX * dt;
                 yaw = yaw + gyroZ * dt;
-
                 Log.d("LOG", "GYROSCOPE           [X]:" + String.format("%.4f", event.values[0])
                         + "           [Y]:" + String.format("%.4f", event.values[1])
                         + "           [Z]:" + String.format("%.4f", event.values[2])
@@ -171,13 +168,11 @@ public class SubActivity extends AppCompatActivity {
                         + "           [Roll]: " + String.format("%.1f", roll * RAD2DGR)
                         + "           [Yaw]: " + String.format("%.1f", yaw * RAD2DGR)
                         + "           [dt]: " + String.format("%.4f", dt));
-
                 // 출력 io 생성
                 ed = (EditText) findViewById(R.id.editText);
                 File file = new File(getFilesDir(), fname);
                 if (!file.exists())
                     return;
-
                 ed.setText("GYROSCOPE\n  [X]:" + String.format("%.4f", event.values[0]) + "\n"
                         + "  [Y]:" + String.format("%.4f", event.values[1]) + "\n"
                         + "  [Z]:" + String.format("%.4f", event.values[2]) + "\n"
@@ -190,7 +185,6 @@ public class SubActivity extends AppCompatActivity {
 
             // 파일에 저장
             cnt++;
-
             if(cnt == 0) {
                 String ess = Environment.getExternalStorageState();
                 String sdCardPath = null;
@@ -201,15 +195,12 @@ public class SubActivity extends AppCompatActivity {
                 } else {
                     showMsg("SD Card not ready!");
                 }
-
                 File file = new File(sdCardPath + "/test1");
                 File file1 = new File(sdCardPath + "/test1/text.txt");
-
                 if (!file.exists()) {
                     file.mkdir();
                     showMsg("기존에없던 폴더가 생성되었습니다.");
                 }
-
                 try {
                     FileOutputStream fos = new FileOutputStream(file1);
                     String msg = "GYROSCOPE\n[X]:" + "[Y]" + "[Z]"+ "[Pitch]" + "[Roll]"+ "[Yaw]"+"[dt]";
@@ -232,19 +223,11 @@ public class SubActivity extends AppCompatActivity {
                 } else {
                     showMsg("SD Card not ready!");
                 }
-
                 String logPath = sdCardPath + "/test1/text.txt"; //파일 경로
                 File templog = new File(logPath);
-
                 try {
                     RandomAccessFile raf = new RandomAccessFile(logPath, "rw"); //이어쓰기용
                     raf.seek(raf.length());//맨마지막 위치로 커서 이동
-                    String str = "GYROSCOPE\n  [X]:" + String.format("%.4f", event.values[0]) + "\n" + "  [Y]:" + String.format("%.4f", event.values[1]) + "\n"
-                            + "  [Z]:" + String.format("%.4f", event.values[2]) + "\n"
-                            + "  [Pitch]: " + String.format("%.1f", pitch * RAD2DGR) + "\n"
-                            + "  [Roll]: " + String.format("%.1f", roll * RAD2DGR) + "\n"
-                            + "  [Yaw]: " + String.format("%.1f", yaw * RAD2DGR) + "\n"
-                            + "  [dt]: " + String.format("%.4f", dt); //기록할 글
                     String str1 = String.format("%.4f", event.values[0]) +
                             "   " + String.format("%.4f", event.values[1]) +
                             "   " + String.format("%.4f", event.values[2]) +
@@ -254,13 +237,13 @@ public class SubActivity extends AppCompatActivity {
                             "   " + String.format("%.4f", dt)+"\n"; //기록할 글
                     raf.writeBytes(str1);
                     raf.close();
-
                 } catch (Exception e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-            }
 
+
+            }
         }
 
         private void showMsg(String msg)
